f580f48d492a4b449a2bd94819d5aec4ca612305
Jiaming <benzclsclass@hotmail.com>
2017-11-24 22:10:44 +0000

    rename access_log to access.log, add read_apache_log_file_into_column.py

---
+++ read_apache_log_file_into_column.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import argparse
+import csv
+import re
+
+
+def extract_fields(line):
+
+    log_line_re = re.compile(r'''(?P<remote_host>\S+) #IP ADDRESS
+                                \s+ #whitespace
+                                (?P<remote_log_name>\S+) #remote log name
+                                \s+ #whitespace
+                                (?P<remote_user>\S+) #remote user
+                                \s+ #whitespace
+                                (?P<time>\[[^\[\]]+\]) #time
+                                \s+ #whitespace
+                                (?P<url>"[^"]+") #first line of request
+                                \s+ #whitespace
+                                (?P<status>\d+)
+                                \s+ #whitespace
+                                (?P<bytes_sent>-|\d+)
+                                \s* #whitespace
+                                ''', re.VERBOSE)
+
+    m = log_line_re.match(line)
+
+    if m:
+        groupdict = m.groupdict()
+        return groupdict
+    else:
+        print("Please check the regular expression, or the log entry to see why they don't match.")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", help="specify a log file to analyse.")
+args = parser.parse_args()
+
+apache_log_file = args.file
+
+with open(apache_log_file, 'r') as fr, open("apache_log_file_analysis.csv", 'w') as fw:
+    for line in fr:
+        fields = extract_fields(line)
+        fieldnames = fields.keys()
+        writer = csv.DictWriter(fw, fieldnames=fieldnames)
+
+        writer.writeheader()
+        writer.writerow(fields)
+
+
